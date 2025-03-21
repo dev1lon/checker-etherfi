7dedfc3e5563281ce845fa90faf78acc64ccb7b0
dev1lon <90638030+dev1lon@users.noreply.github.com>
2025-03-22 02:41:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import asyncio
 from pathlib import Path
 import os
 
-from utils import utils, checker
+from utils import utils, checker, logger
+
+
+logger = logger.get_logger()
 
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -22,7 +25,10 @@ async def main():
     for address, proxy in zip(addresses, proxies):
         tasks.append(checker.checker(address, proxy, semaphore))
 
-    await asyncio.gather(*tasks)
+    results = await asyncio.gather(*tasks)
+
+    eligible_count = sum(1 for result in results if result)
+    logger.success(f'Eligible wallets - {eligible_count}')
 
 if __name__ == '__main__':
   asyncio.run(main())
